52cceafeb683eaa7a484d6f5894fb93f40183b84
fraidyEfrat <yafen1818@gmail.com>
2023-12-26 12:33:00 +0200

    commits to api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-import { User, Post } from './types';
+import { User, Post } from './types';// Import types for data structuring
 
+// Create an Axios instance with a base URL for API requests
 const api = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
 });
 
+// Function to fetch users from the API
 export const fetchUsers = async () => {
   try {
     const response = await api.get<User[]>('/users');
@@ -15,12 +17,13 @@ export const fetchUsers = async () => {
   }
 };
 
+
 export const fetchPostsByUserId = async (userId: number) => {
   try {
-    const response = await api.get<Post[]>(`/posts?userId=${userId}`);
-    return response.data;
+    const response = await api.get<Post[]>(`/posts?userId=${userId}`);// GET request with userId parameter
+    return response.data;// Return the parsed post data
   } catch (error) {
-    throw new Error('Failed to fetch posts');
+    throw new Error('Failed to fetch posts');// Handle errors
   }
 };
 
@@ -32,7 +35,6 @@ export const createPost = async (postData: Post): Promise<Post> => {
     console.log('Post created:', createdPost);
     return createdPost;
   } catch (error) {
-    console.error('Error creating post:', error);
     throw new Error('Failed to create post');
   }
 
