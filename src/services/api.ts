aaf453fd3ae7b2f64de651180ef81580bebafb25
fraidyEfrat <yafen1818@gmail.com>
2023-12-26 12:25:05 +0200

    adjustment the create to axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import { User ,Post} from './types';
+
+import { User, Post } from './types';
+
 const api = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
 });
@@ -21,37 +23,17 @@ export const fetchPostsByUserId = async (userId: number) => {
     throw new Error('Failed to fetch posts');
   }
 };
+
 export const createPost = async (postData: Post): Promise<Post> => {
   try {
-    // Simulate post creation using the fetch API
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify(postData),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
+    const response = await api.post<Post>('/posts', postData);
+    const createdPost = response.data;
 
-    // Check if the request was successful 
-    if (response.ok) {
-    // Modify the returned data to include the original postData
-      const createdPost: Post = {
-        ...postData,
-        userId: postData.userId || 1, // Set userId to postData.userId if available, or default to 1
-        id: Math.floor(Math.random() * 1000), // Generate a random ID 
-      };
-      
-      console.log('Post created:', createdPost);
-      return createdPost;
-    } else {
-      // Handle errors 
-      console.error('Failed to create post:', response.status, response.statusText);
-      throw new Error('Failed to create post');
-    }
+    console.log('Post created:', createdPost);
+    return createdPost;
   } catch (error) {
     console.error('Error creating post:', error);
     throw new Error('Failed to create post');
   }
+
 };
-
-
