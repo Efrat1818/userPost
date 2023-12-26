1ebcc2ae770c52544c839e26b299fc47ce6a8e95
fraidyEfrat <yafen1818@gmail.com>
2023-12-26 12:11:33 +0200

    delete commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -32,19 +32,19 @@ export const createPost = async (postData: Post): Promise<Post> => {
       },
     });
 
-    // Check if the request was successful (status code 2xx)
+    // Check if the request was successful 
     if (response.ok) {
     // Modify the returned data to include the original postData
       const createdPost: Post = {
         ...postData,
         userId: postData.userId || 1, // Set userId to postData.userId if available, or default to 1
-        id: Math.floor(Math.random() * 1000), // Generate a random ID (replace this with your logic)
+        id: Math.floor(Math.random() * 1000), // Generate a random ID 
       };
       
       console.log('Post created:', createdPost);
       return createdPost;
     } else {
-      // Handle errors (status code other than 2xx)
+      // Handle errors 
       console.error('Failed to create post:', response.status, response.statusText);
       throw new Error('Failed to create post');
     }
@@ -53,33 +53,5 @@ export const createPost = async (postData: Post): Promise<Post> => {
     throw new Error('Failed to create post');
   }
 };
-// Placeholder for createPost since the API doesn't allow actual creation
-// export const createPost = async (postData: Post) => {
-//   try {
-//     // Simulate post creation using the fetch API
-//     ///////////!!!!!!!!!!!!!!!!!!!!!!!https://jsonplaceholder.typicode.com/posts??????short
-//     const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-//       method: 'POST',
-//       body: JSON.stringify(postData),
-//       headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//       },
-//     });
-
-//     // Check if the request was successful (status code 2xx)
-//     if (response.ok) {
-//       const createdPost = await response.json();
-//       console.log('Post created:', createdPost);
-//       return createdPost;
-//     } else {
-//       // Handle errors (status code other than 2xx)
-//       console.error('Failed to create post:', response.status, response.statusText);
-//       throw new Error('Failed to create post');
-//     }
-//   } catch (error) {
-//     console.error('Error creating post:', error);
-//     throw new Error('Failed to create post');
-//   }
-// };
 
 
